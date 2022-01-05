b215724ffcece0f83077e051afa3edc1294a2a02
Kjell Derous <kjell.derous@accentjobs.be>
2022-01-05 08:12:14 +0100

    Add end to end tests

---
+++ src/Customers.API.E2ETests/Customers/GetCustomerTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+
+namespace Customers.API.E2ETests.Customers
+{
+    [TestClass]
+    public class GetCustomerTests
+    {
+        [TestMethod]
+        public async Task Get_Customer()
+        {
+            // Arrange
+            var client = new RestClient("https://localhost:5001/api/");
+            var request = new RestRequest("customers/1");
+
+            // Act
+            var response = await client.ExecuteAsync(request);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+
+        [TestMethod]
+        public async Task Get_NonExisting_Customer()
+        {
+            // Arrange
+            var client = new RestClient("https://localhost:5001/api/");
+            var request = new RestRequest($"customers/{int.MaxValue}");
+
+            // Act
+            var response = await client.ExecuteAsync(request);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
---
+++ src/Customers.API.E2ETests/Customers/GetCustomersTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+
+namespace Customers.API.E2ETests.Customers
+{
+    [TestClass]
+    public class GetCustomersTests
+    {
+        [TestMethod]
+        public async Task Get_Customers()
+        {
+            // Arrange
+            var client = new RestClient("https://localhost:5001/api/");
+            var request = new RestRequest("customers");
+
+            // Act
+            var response = await client.ExecuteAsync(request);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+    }
+}
